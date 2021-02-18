45c5342b4c3b28b7347d540b86b8730edefadf14
RoyalOttoc <jong.kim8706@gmail.com>
2021-02-19 00:27:05 +1100

    update animation

--- main.js
+++ main.js
@@ -66,12 +66,38 @@ document.addEventListener('scroll', ()=>{
 arrowBtn.addEventListener('click', ()=>{
     scrollIntoView("#home")
 })
+// remove selection from the previous item and select
+
+// Projects
+const workBtnContainer = document.querySelector('.work__categories');
+const projectContainer = document.querySelector('.work__projects');
+const projects = document.querySelectorAll('.project');
+workBtnContainer.addEventListener('click', (e)=>{
+    const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+    if(filter == null){
+        return;
+    }
+    projectContainer.classList.add('anim-out');
+    setTimeout(()=> {
+        projects.forEach((project) => {
+            if(filter === '*' || filter === project.dataset.type){
+                project.classList.remove('invisible');
+            } else {
+                project.classList.add('invisible');
+            }
+    });
+        projectContainer.classList.remove('anim-out') 
+
+    }, 300)
+
 
 function scrollIntoView(selector){
     const scrollTo = document.querySelector(selector);
     scrollTo.scrollIntoView({behavior: 'smooth'})
 }
 
+    
+})
 
 
 
